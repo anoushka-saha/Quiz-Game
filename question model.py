9d9d42383d952a6706e0a8395565872c80dad5a3
anoushka1111 <saha.anoushka@gmail.com>
2024-05-13 22:34:59 -0400

    part 1 - creating question class

---
+++ question model.py
@@ -0,0 +1,12 @@
+#Anoushka Saha
+#Quiz Game
+#Question Model Object
+#May 13th, 2024
+#Day 17 Final Project
+
+#Attributes: text, answer
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
